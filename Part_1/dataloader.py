4293191613f53b2142ba20b90d1f866fd9f69118
kamsur <kamsur.rahaman@gmail.com>
2024-11-09 13:12:36 +0100

    corrections

--- Part_1/dataloader.py
+++ Part_1/dataloader.py
@@ -9,14 +9,15 @@ import time
 train_dataset = CustomDataset(data_type="jpeg", mode="train")
 batch_size = 100
 print("Batch size:", batch_size)
+loading_times = []
 for infant_age in range(4):
     train_dataset.infant_age = infant_age
     if infant_age < 3:
         print(
-            f"Vision for {train_dataset.infant_age} month old infant:",
+            f"\n\nVision for {train_dataset.infant_age} month old infant:",
         )
     else:
-        print("Adult Vision:")
+        print("\n\nAdult Vision:")
 
     train_dl = t.utils.data.DataLoader(
         train_dataset,
@@ -25,34 +26,24 @@ for infant_age in range(4):
         shuffle=True,
         num_workers=1,
     )
-    loading_times = []
+
     loading_start_time = time.time()
-    for i, (x, y) in enumerate(train_dl):
-        current_time = time.time()
-        loading_times.append(current_time - loading_start_time)
-        print(f"Batch {i} took {loading_times[-1]} seconds to load")
-        if i == 3:
-            break
-        loading_start_time = time.time()
-    avg_loading_time = sum(loading_times) / len(loading_times)
+    (x, y) = next(iter(train_dl))
+    current_time = time.time()
+    average_loading_time = (current_time - loading_start_time) / batch_size
+    loading_times.append(average_loading_time)
+
     if infant_age < 3:
         print(
             f"Average loading time for {train_dataset.infant_age} months:",
-            avg_loading_time,
+            average_loading_time,
         )
-        plt.figure()
-        plt.plot(loading_times)
-        plt.axhline(y=avg_loading_time, color="r", linestyle="--")
-        plt.title(f"Infant age: {train_dataset.infant_age} months")
-        plt.xlabel("Batch")
-        plt.ylabel("Loading time (s)")
-        plt.savefig(f"infant_age_{train_dataset.infant_age}_loading_time.png")
     else:
-        print("Average loading time for adult vision:", avg_loading_time)
-        plt.figure()
-        plt.plot(loading_times)
-        plt.axhline(y=avg_loading_time, color="r", linestyle="--")
-        plt.title("Adult vision")
-        plt.xlabel("Batch")
-        plt.ylabel("Loading time (s)")
-        plt.savefig("adult_loading_time.png")
+        print("Average loading time for adult vision:", average_loading_time)
+
+plt.figure()
+plt.plot(loading_times)
+plt.title("Loading time for different ages (>2 months is perfect vision)")
+plt.xlabel("Infant age (months)")
+plt.ylabel("Loading time (s)")
+plt.savefig("Loading_times.png")
